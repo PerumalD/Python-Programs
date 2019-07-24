b7e0e4ff8de00486b08efdeaf3aa0b6d0aa68563
Perumal <33823888+PerumalD@users.noreply.github.com>
2019-07-24 22:38:56 +0530

    Add files via upload

---
+++ Randomsum.py
@@ -0,0 +1,66 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 10,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "237\n",
+      "12\n"
+     ]
+    }
+   ],
+   "source": [
+    "from random import random\n",
+    "\n",
+    "n = random() * 900 + 100\n",
+    "n = int(n)\n",
+    "print(n)\n",
+    "\n",
+    "a = n // 100\n",
+    "b = (n // 10) % 10\n",
+    "c = n % 10\n",
+    "print(a + b + c)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
---
+++ oddeven.py
@@ -0,0 +1,60 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "metadata": {},
+   "outputs": [
+    {
+     "ename": "SyntaxError",
+     "evalue": "invalid syntax (<ipython-input-4-83c0b606d1cf>, line 10)",
+     "output_type": "error",
+     "traceback": [
+      "\u001b[0;36m  File \u001b[0;32m\"<ipython-input-4-83c0b606d1cf>\"\u001b[0;36m, line \u001b[0;32m10\u001b[0m\n\u001b[0;31m    else a<= c >= b:\u001b[0m\n\u001b[0m         ^\u001b[0m\n\u001b[0;31mSyntaxError\u001b[0m\u001b[0;31m:\u001b[0m invalid syntax\n"
+     ]
+    }
+   ],
+   "source": [
+    "a = int(input(\"Enter the Number1:\"))\n",
+    "b = int(input(\"Enter the Number2:\"))\n",
+    "c = int(input(\"Enter the Number3:\"))\n",
+    "\n",
+    "print(\"The Maixmum of Three Numbers:\", end=\"\")\n",
+    "if b <= a >= c:\n",
+    "    print(a)\n",
+    "elif a <= b >= c:\n",
+    "    print(b)\n",
+    "elif a<= c >= b:\n",
+    "    print(c)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
